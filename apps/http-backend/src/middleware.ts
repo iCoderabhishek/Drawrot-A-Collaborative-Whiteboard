28bc807810085f093c784bc4e3e341e090907b39
Abhishek Jha <iamabhishek1310@gmail.com>
2025-10-27 02:42:37 +0530

    added http backend and db migrations

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,16 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
+import {JWT_SECRET} from "@repo/backend-common/config"
 
-const JWT_SECRET = process.env.JWT_SECRET || "11111"
 
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers["authorization"] ?? ""
 
-    const decoded = jwt.verify(token, JWT_SECRET) as {username: string}
+    const decoded = jwt.verify(token, JWT_SECRET) as {userId: string}
 
     if (decoded){
-        req.userId = decoded.username 
+        req.userId = decoded.userId
         next()
     } else {
         res.status(403).json({
